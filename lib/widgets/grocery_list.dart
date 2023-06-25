1c69103426ba7cd410be09440278efa13795a14b
abdullah <stay__away121@hotmail.com>
2023-06-25 16:46:22 +0300

    4:47

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,14 +10,28 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(
+  final List<GroceryItem> _groceryItems = [];
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
         builder: (ctx) {
           return const NewItem();
         },
       ),
     );
+    if (newItem == null) {
+      return;
+    } else {
+      setState(() {
+        _groceryItems.add(newItem);
+      });
+    }
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
   }
 
   @override
@@ -27,19 +41,31 @@ class _GroceryListState extends State<GroceryList> {
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: ListView.builder(
-          itemCount: groceryItems.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(groceryItems[index].name),
-              leading: Container(
-                width: 24,
-                height: 24,
-                color: groceryItems[index].category.color,
-              ),
-              trailing: Text('${groceryItems[index].quantity}'),
-            );
-          }),
+      body: _groceryItems.isEmpty
+          ? const Center(
+              child: Text(
+              'No Items here!',
+              style: TextStyle(fontSize: 30),
+            ))
+          : ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (context, index) {
+                return Dismissible(
+                  key: ValueKey(_groceryItems[index].id),
+                  onDismissed: (direction) {
+                    _removeItem(_groceryItems[index]);
+                  },
+                  child: ListTile(
+                    title: Text(_groceryItems[index].name),
+                    leading: Container(
+                      width: 24,
+                      height: 24,
+                      color: _groceryItems[index].category.color,
+                    ),
+                    trailing: Text('${_groceryItems[index].quantity}'),
+                  ),
+                );
+              }),
     );
   }
 }
