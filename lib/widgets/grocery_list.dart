0105147efc80672be3d690af4bd0043eb098947e
abdullah <stay__away121@hotmail.com>
2023-06-27 12:36:31 +0300

    adding features

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+import 'package:shopping_list/data/categories.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
 
 class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
@@ -10,7 +13,38 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  List<GroceryItem> _groceryItems = [];
+  var _isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    _loadItems();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-7df2e-default-rtdb.europe-west1.firebasedatabase.app',
+        'shopping-list.json');
+    final response = await http.get(url);
+    final Map<String, dynamic> listData = json.decode(response.body);
+    final List<GroceryItem> loadedItems = [];
+    for (final item in listData.entries) {
+      final category = categories.entries
+          .firstWhere(
+              (element) => item.value['category'] == element.value.title)
+          .value;
+      loadedItems.add(GroceryItem(
+          id: item.key,
+          name: item.value['name'].toString(),
+          quantity: item.value['quantity'],
+          category: category));
+    }
+    setState(() {
+      _groceryItems = loadedItems;
+      _isLoading = false;
+    });
+  }
+
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
       MaterialPageRoute(
@@ -36,36 +70,43 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+        child: Text(
+      'No Items here!',
+      style: TextStyle(fontSize: 30),
+    ));
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+          itemCount: _groceryItems.length,
+          itemBuilder: (context, index) {
+            return Dismissible(
+              key: ValueKey(_groceryItems[index].id),
+              onDismissed: (direction) {
+                _removeItem(_groceryItems[index]);
+              },
+              child: ListTile(
+                title: Text(_groceryItems[index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: _groceryItems[index].category.color,
+                ),
+                trailing: Text('${_groceryItems[index].quantity}'),
+              ),
+            );
+          });
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
         actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
-      body: _groceryItems.isEmpty
-          ? const Center(
-              child: Text(
-              'No Items here!',
-              style: TextStyle(fontSize: 30),
-            ))
-          : ListView.builder(
-              itemCount: _groceryItems.length,
-              itemBuilder: (context, index) {
-                return Dismissible(
-                  key: ValueKey(_groceryItems[index].id),
-                  onDismissed: (direction) {
-                    _removeItem(_groceryItems[index]);
-                  },
-                  child: ListTile(
-                    title: Text(_groceryItems[index].name),
-                    leading: Container(
-                      width: 24,
-                      height: 24,
-                      color: _groceryItems[index].category.color,
-                    ),
-                    trailing: Text('${_groceryItems[index].quantity}'),
-                  ),
-                );
-              }),
+      body: content,
     );
   }
 }
